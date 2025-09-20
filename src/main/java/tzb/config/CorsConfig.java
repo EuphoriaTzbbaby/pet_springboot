4db1246f19828bb28c02aa0ee082bd7474e2644c
EuphoriaTzbbaby <3301344200@qq.com>
2025-09-20 23:11:00 +0800

    cnm

--- src/main/java/tzb/config/CorsConfig.java
+++ src/main/java/tzb/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://cww.cwwdka.com", "http://cww.cwwdka.com", "http://cwwdka.com:9999", "http://47.101.42.177:9999", "http://localhost:5173", "http://localhost:8080", "http://localhost:5174")
+                .allowedOrigins("https://mmd.cwwdka.com", "http://mmd.cwwdka.com", "http://cwwdka.com:9999", "http://47.101.42.177:9999", "http://localhost:5173", "http://localhost:8080", "http://localhost:5174")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true)
                 .maxAge(3600);
